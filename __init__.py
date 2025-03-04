b4ed7612506b20209c822991735d2863187d3c5e
bokujuu <66839053+bokujuu@users.noreply.github.com>
2025-03-04 21:10:22 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,19 +2,21 @@ from .image_sequence_loader import ImageSequenceLoader
 from .image_pair_sequence_loader import ImagePairSequenceLoader
 from .text_combiner import TextCombiner
 from .text_processor import TextProcessor
-from .lora_loader_elemental import LoraLoaderElemental
+#from .lora_loader_elemental import LoraLoaderElemental
 from .random_word_replacer import RandomWordReplacer
 from .lora_weight_randomizer import LoraWeightRandomizer
-
+from .easy_lora_weight_randomizer import EasyLoraWeightRandomizer
 
 NODE_CLASS_MAPPINGS = {
     "ImageSequenceLoader": ImageSequenceLoader,
     "ImagePairSequenceLoader": ImagePairSequenceLoader,
     "TextCombiner": TextCombiner,
     "TextProcessor": TextProcessor,
-    "LoraLoaderElemental": LoraLoaderElemental,
+    #"LoraLoaderElemental": LoraLoaderElemental,
     "RandomWordReplacer": RandomWordReplacer,
     "LoraWeightRandomizer": LoraWeightRandomizer,
+    "EasyLoraWeightRandomizer": EasyLoraWeightRandomizer,
+
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -22,9 +24,10 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImagePairSequenceLoader": "Image Pair Sequence Loader",
     "TextCombiner": "Text Combiner",
     "TextProcessor": "Text Processor",
-    "LoraLoaderElemental": "Lora Loader Elemental",
+    #"LoraLoaderElemental": "Lora Loader Elemental",
     "RandomWordReplacer": "Random Word Replacer",
     "LoraWeightRandomizer": "Lora Weight Randomizer",
+    "EasyLoraWeightRandomizer": "Easy Lora Weight Randomizer",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
